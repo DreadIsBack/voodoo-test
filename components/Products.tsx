b86dd21eb3bb503191e275d8a5ec67b5874bb3a4
Alexander <DreadIsBack@users.noreply.github.com>
2023-05-24 18:50:39 +0300

    Implement response design for products

--- components/Products.tsx
+++ components/Products.tsx
@@ -1,8 +1,24 @@
+import classNames from "classnames";
 import ProductCard from "./ProductCard";
+import "./Products.scss";
 
-export default function Products() {
+interface Props {
+  className?: string;
+}
+
+export default function Products(props: Props) {
+  const rootClass = classNames(
+    props.className,
+    "grid gap-x-6 products justify-center"
+  );
+
   return (
-    <div>
+    <div className={rootClass}>
+      <ProductCard />
+      <ProductCard />
+      <ProductCard />
+      <ProductCard />
+      <ProductCard />
       <ProductCard />
       <ProductCard />
       <ProductCard />
