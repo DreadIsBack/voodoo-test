ac566d6442a7b138108ea76d44d838dff2cf90b2
Alexander <DreadIsBack@users.noreply.github.com>
2023-05-24 18:50:56 +0300

    Implement responsive design for subscribe

--- components/Subscribe.tsx
+++ components/Subscribe.tsx
@@ -2,13 +2,13 @@ export default function Subscribe() {
   return (
     <div className="mt-[98px] text-center text-sm">
       <p>Be the first one to know when we launch our beta!</p>
-      <div className="mt-6">
+      <div className="mt-6 flex justify-center gap-3">
         <input
           type="email"
-          className="p-4 font-semibold placeholder:text-black"
+          className="p-4 w-full max-w-[518px] font-semibold placeholder:text-black"
           placeholder="E-mail"
         />
-        <button className="ml-3 p-4 rounded bg-[#ED5E21] font-semibold">
+        <button className="p-4 shrink-0 rounded bg-[#ED5E21] font-semibold">
           Sign-up
         </button>
       </div>
