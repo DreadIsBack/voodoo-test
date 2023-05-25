7438e2c8b693d501554045713bffb01e5d152408
Alexander <DreadIsBack@users.noreply.github.com>
2023-05-25 09:56:12 +0300

    Extract method

--- components/Products.tsx
+++ components/Products.tsx
@@ -10,23 +10,25 @@ interface Props {
   className?: string;
 }
 
+async function fetchProducts(): Promise<Product[]> {
+  return fetch("https://voodoo-sandbox.myshopify.com/products.json?limit=12")
+    .then((response) => response.json())
+    .then((response) =>
+      response.products.map((x: any) => ({
+        id: x.id,
+        title: x.title,
+        imageSrc: x.images[0]?.src ?? "",
+        price: x.variants[0].price,
+      }))
+    );
+}
+
 export default function Products(props: Props) {
   const [products, setProducts] = useState<Product[] | null>(null);
 
   useEffect(() => {
     if (!products) {
-      fetch("https://voodoo-sandbox.myshopify.com/products.json?limit=12")
-        .then((response) => response.json())
-        .then((response) =>
-          setProducts(
-            response.products.map((x: any) => ({
-              id: x.id,
-              title: x.title,
-              imageSrc: x.images[0]?.src ?? "",
-              price: x.variants[0].price,
-            }))
-          )
-        );
+      fetchProducts().then((response) => setProducts(response));
     }
   });
 
